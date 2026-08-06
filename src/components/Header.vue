5598a5e3cb8d42c978d8a13fe8d691910e2b9dba
jasurbekravshanbekov826-ship-it <jasurbekravshanbekov826@gmail.com>
2026-08-06 15:11:50 +0500

    togilangan

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,7 +17,7 @@
         <router-link to="/contact" class="transition hover:text-sky-300">Contacts</router-link>
       </nav>
 
-      <button class="rounded-md border border-white bg-white px-4 py-2 text-sm font-medium transition hover:bg-white/20">
+      <button class="rounded-md border border-white bg-white px-4 py-2 text-sm font-medium transition hover:bg-white">
         Login
       </button>
     </div>
